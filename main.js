806c0984d7ff4c80e47d3ee052b29bd58109e5a4
maylimachi <maylimachi18@gmail.com>
2025-10-31 11:27:36 -0300

    Modificando boton "ver mas" de las cards

--- main.js
+++ main.js
@@ -18,3 +18,18 @@ document.querySelector("form").addEventListener("submit", function(event) {
         console.error(err);
     });
 });
+
+function toggleCard(button) {
+  const card = button.closest('.card');
+  const icon = button.querySelector('i');
+
+  // Alternar clase show-info
+  card.classList.toggle('show-info');
+
+  // Cambiar texto del botón
+  if (card.classList.contains('show-info')) {
+    button.innerHTML = 'Ver menos <i class="bi bi-caret-up-fill"></i>';
+  } else {
+    button.innerHTML = 'Ver más <i class="bi bi-caret-down-fill"></i>';
+  }
+}
